a6cce0973f7422ea296428d500cef9dad9c0aa14
Evgeniy1109 <q126p@yandex.ru>
2023-06-02 16:57:37 +0300

    quasar

---
+++ quasar-project/src/pages/Help.vue
@@ -0,0 +1,10 @@
+<template>
+  <q-page class="q-pa-lg">
+    <h3>Помощь</h3>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores accusantium, iusto inventore ad mollitia sed debitis. Adipisci pariatur atque aperiam vel? Quibusdam culpa corrupti laborum mollitia! Sapiente nihil iste qui.</p>
+  </q-page>
+</template>
+
+<script>
+
+</script>
